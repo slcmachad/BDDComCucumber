07a0af57f40f66eccb7c47607774e0b6211bbaae
Salomao <salomao.machado.75@gmail.com>
2023-07-15 22:59:35 -0300

    <feat> Criando as verificações para quando o teste verificar se o usuario é valido.

--- src/test/java/br/com/alura/leilao/acceptance/steps/LoginSteps.java
+++ src/test/java/br/com/alura/leilao/acceptance/steps/LoginSteps.java
@@ -1,21 +1,35 @@
 package br.com.alura.leilao.acceptance.steps;
 
+import org.junit.Assert;
+
+import br.com.alura.leilao.e2e.pages.Browser;
+import br.com.alura.leilao.e2e.pages.LeiloesPage;
+import br.com.alura.leilao.e2e.pages.LoginPage;
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.Entao;
 import io.cucumber.java.pt.Quando;
 
 public class LoginSteps {
 
+	private Browser browser;
+	private LoginPage loginPage;
+	private LeiloesPage leiloesPage;
+
 	@Dado("o usuario valido")
 	public void o_usuario_valido() {
+		browser = new Browser();
+		browser.seed();
+		loginPage = browser.getLoginPage();
 	}
 
 	@Quando("realiza login")
 	public void realiza_login() {
+		leiloesPage = this.loginPage.realizaLoginComo("fulano", "pass");
 	}
 	
 	@Entao("é redirecionado para a pagina de leiloes")
 	public void é_redirecionado_para_a_pagina_de_leiloes() {
+		Assert.assertTrue(this.leiloesPage.estaNaPaginaDeLeiloes());
 	}
 	
 	@Dado("o usuario invalido")
